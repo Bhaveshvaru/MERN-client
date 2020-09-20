6bddcedf4b5090c8dbc85f0b23eed1e366e0e39d
BhaveshVaru <bhavesh.varu97@gmail.com>
2020-09-20 15:50:30 +0530

    added navbar and admin page router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './container/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Signin from './container/Signin';
+import Signup from './container/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/signin" component={Signin} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </div>
   );
 }
